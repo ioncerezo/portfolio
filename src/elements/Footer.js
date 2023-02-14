ac64b74abcd0395bbf58e680750c917e3ae027d1
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-14 17:55:26 +0000

    fix footer

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -4,7 +4,6 @@ function Footer(){
     <hr className='mb-3 border-black max-w-lg border-1 mx-auto lg:my-8' />
     <div className='flex flex-col items-center gap-3 justify-center flex-wrap'>
         <p className='text-sm  md:font-md'>© 2023 Ion Cerezo - <a href='mailto:ioncerezovalero@gmail.com' rel='noreferrer' target='_blank'className='hover:underline'>ioncerezovalero@gmail.com</a> - +34 644 97 18 68</p>
-        <p className='text-xs italic '>Página desarrollada con React y TailwindCSS</p>
     </div>
 </footer>
     )
